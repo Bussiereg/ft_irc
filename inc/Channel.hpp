628842e118ed209a3c6487832bd64d6ac0729152
Guillaume Bussiere <gbussier@c4c3c1.42berlin.de>
2024-05-20 20:06:02 +0200

    implementation of modes, topics, operators and client list

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -1,32 +1,24 @@
 #pragma once
-#include <iostream>
-#include <vector>
-#include <string>
-#include <stdexcept>
-#include <poll.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <cstdlib>
-#include <fcntl.h>
-#include <cstring>
+#include <map>
 #include <algorithm>
-#include <utility>
 #include "Client.hpp"
 #include "replies.hpp"
+#include "Server.hpp"
+
 
 class Channel
 {
 private:
 	std::string _channelName;
+	std::map<char, bool> _mode;
+	std::string _topic;
+	std::vector<Client*> _clientList;
+	std::vector<Client*> _operator;
 public:
-	Channel(std::string name);
+	Channel(std::string & name, Client & client);
 	~Channel();
+	std::string	getTopic() const;
+	void	setTopic(Client & lhs, std::string & newTopic);
+	void	setChannelMode(char mode, bool status);
+	std::string getChannelName() const;
 };
-
-Channel::Channel(std::string name) : _channelName(name){
-}
-
-Channel::~Channel()
-{
-}
