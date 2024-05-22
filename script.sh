c3ca2aad5e39170d557e7400b22eaffdf190ef18
Jules <jbjulesbernard3@gmail.com>
2024-05-22 12:54:10 +0200

    x Revert "Merge pull request #14 from Bussiereg/ping-pong"
    This reverts commit 0e67c27f2e30d14069b12b380cf24e30d55e0305, reversing
    changes made to 20b365516e0df7853243be60c645e4bbce5c9ca1.

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# Open a new terminal window with gnome-terminal and execute irssi commands
-gnome-terminal -- bash -c "irssi -n Ono -c localhost -p 6667 pw"
-gnome-terminal -- bash -c "irssi -n Twingo -c localhost -p 6667 pw"
-gnome-terminal -- bash -c "irssi -n Treacy -c localhost -p 6667 pw"
-
-
