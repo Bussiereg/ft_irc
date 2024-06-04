bb56be8a308a1d05dc6048cc075734d153f9051d
Guillaume <guillaume.bussiere@live.fr>
2024-06-04 16:18:37 +0200

    the invite messqge weren't send corectly

--- src/channelOperations.cpp
+++ src/channelOperations.cpp
@@ -93,6 +93,7 @@ void Server::_handleInviteCommand(Client &client, std::string &input)
 	}
 
 	channel->setInviteList(invitee);
+	// client.appendResponse(RPL_INVITING(_serverName, nick, channelName, invitee));
 	(*clientInvitee)->appendResponse(INVITE(nick, client.getUsername(), client.getHostname(),  invitee, channelName));	
 }
 
