441a26c74d0810f470f3d4326b5ebe9a7bde891b
Guillaume <guillaume.bussiere@live.fr>
2024-06-04 13:22:50 +0200

    clean the code

--- chatbot/inc/bot.hpp
+++ chatbot/inc/bot.hpp
@@ -10,22 +10,27 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <ctime>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 class bot
 {
 private:
-	int _numberToGuess;
-
-	std::string		_serverName;
-    unsigned int	_port;
-    std::string		_nickBot;
-    std::string		_userBot;
-	std::string		_password;
-	void 			_sendCommand(const std::string& cmd);
-	int				_generateRandomNumber();
-	void 			_wakeUp();
-	void 			_handleServerMessage(const std::string& message);
-	std::string		_getMessageFromUser(const std::string& input);
+	int 						_numberToGuess;
+	std::string					_serverName;
+    unsigned int				_port;
+    std::string					_nickBot;
+    std::string					_userBot;
+	std::string					_password;
+	void 						_sendCommand(const std::string& cmd);
+	int							_generateRandomNumber();
+	void 						_wakeUp();
+	void 						_handleServerMessage(const std::string& message);
+	std::string					_getMessageFromUser(const std::string& input);
+	void						_greeting(std::string & sender, std::string & input);
+	void 						_letsPlay(std::string & sender, std::string & input);
+	std::vector<std::string> 	_listOfUser;
 public:
 	bot(unsigned int, std::string);
 	~bot();
